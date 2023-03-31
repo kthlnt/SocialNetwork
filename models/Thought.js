6a2c35c06cb3f9ebf92020b53867f222c73636f2
Katherine Lantto <kathlantto@gmail.com>
2023-03-31 15:54:58 -0700

    cloned a similar project for starter code, began working on models

---
+++ models/Thought.js
@@ -0,0 +1,74 @@
+const { Schema, Types } = require('mongoose');
+//moment adds timestamp
+const moment = require('moment')
+//thoughts schema created
+const thoughtSchema = new Schema (
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true,
+      },
+      id: false,
+  }
+)
+
+//reaction schema created
+const reactionSchema = new Schema (
+  {
+     reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+     },
+     reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+     },
+     username: {
+      type: String,
+      required: true,
+     },
+     createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+     },
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false,
+  }
+)
+
+
+
+
+//retrieves length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+
+module.exports = thoughtSchema;
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
